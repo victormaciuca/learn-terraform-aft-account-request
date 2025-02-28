a413a7f10296f3510d7d47eeb688a2a4c9439820
Victor <victor.maciuca@gmail.com>
2025-02-28 14:51:14 +0200

    Add sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "victor.maciuca+sandbox.account@gmail.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "victor.maciuca+sandbox.account@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
@@ -25,3 +25,4 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+//testfor pus
